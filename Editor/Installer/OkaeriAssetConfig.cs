b9d01842b9315b9c5e2e29f39f622bfda8648cf4
PinkRammy <victor.vrabete@outlook.com>
2023-07-29 01:55:43 +0300

    Update asset configuration file

--- Editor/Installer/OkaeriAssetConfig.cs
+++ Editor/Installer/OkaeriAssetConfig.cs
@@ -3,23 +3,26 @@ using UnityEngine;
 
 namespace Okaeri.Editor
 {
+    public class OkaeriAssetRequiredAttribute : Attribute { }
+
     [CreateAssetMenu(fileName = "AssetConfig", menuName = "Okaeri/Installer/Asset Configuration")]
     public class OkaeriAssetConfig : ScriptableObject
     {
-        public string AssetName;
-        public string AssetPath;
+        [OkaeriAssetRequired] public string AssetName;
+        [OkaeriAssetRequired] public string AssetPath;
 
-        public string BoothURL = "https://okaeri-shop.booth.pm";
-        public string GumroadURL = "https://gum.okaeri.moe";
+        [OkaeriAssetRequired] public string BoothURL = "https://okaeri-shop.booth.pm";
+        [OkaeriAssetRequired] public string GumroadURL = "https://gum.okaeri.moe";
 
-        public string PrefabName;
-        public string AssetItemName;
+        [OkaeriAssetRequired] public string PrefabName;
+        [OkaeriAssetRequired] public string AssetItemName;
 
         public string AssetFXAnimatorWDOff;
         public string AssetFXAnimatorWDOn;
 
         public string AssetExpressionParams;
         public string AssetExpressionsMenu;
+        public string AssetMaterialsFolder;
 
         public string[] MovableItems;
         public string[] ScalableItems;
